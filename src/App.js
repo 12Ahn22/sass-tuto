e3790ba811f5817a8f21f8f94a5743e8c8da5e4a
12Ahn22 <44540726+12Ahn22@users.noreply.github.com>
2021-03-16 16:42:44 +0900

    styled component fundamental

--- src/App.js
+++ src/App.js
@@ -3,22 +3,44 @@ import './App.scss';
 import CheckBox from './component/CheckBox';
 import { useState } from 'react';
 
-function App() {
-	const [check, setCheck] = useState('');
-	const onChange = (e) => {
-		setCheck(e.target.checked);
-	};
+// styled-component
+import styled, { css } from 'styled-components';
 
+const Circle = styled.div`
+	width: 5rem;
+	height: 5rem;
+	background: ${(props) => props.color};
+	border-radius: 50%;
+	${(props) =>
+		props.huge &&
+		css`
+			width: 10rem;
+			height: 10rem;
+		`}
+`;
+
+function App() {
 	return (
 		<div>
-			<CheckBox checked={check} onChange={onChange}>
-				다음 약관에 모두 동의
-			</CheckBox>
+			<Circle color="blue" huge />
 		</div>
 	);
 }
 
 export default App;
+
+// const [check, setCheck] = useState('');
+// 	const onChange = (e) => {
+// 		setCheck(e.target.checked);
+// 	};
+
+// return (
+// 		<div>
+// 			<CheckBox checked={check} onChange={onChange}>
+// 				다음 약관에 모두 동의
+// 			</CheckBox>
+// 		</div>
+// 	);
 
 // return (
 // 		<div className="App">
